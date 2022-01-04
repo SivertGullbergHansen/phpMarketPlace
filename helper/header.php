a2a9df04688dac9f9aaeeffec7c2f27d1031cc7f
Sivert <sivertgh@stud.ntnu.no>
2022-01-04 03:10:25 +0100

    Added "demo"-disclaimer

--- helper/header.php
+++ helper/header.php
@@ -7,9 +7,10 @@
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
-    if (isset($_SESSION["user_id"]))
+    if (isset($_SESSION["user_id"])) {
         echo "<input type='text' name='search' id='search' placeholder='Search'><div id='logBtn'><p id='playerCurrency'>" . $_SESSION['userCurrency'] . "</p><a href='account.php' id='playerName'>" . $_SESSION['username'] . "</a><a href='logout.php'>Logout</a></div>";
-    else
+        echo "<div class='alert'><p>This is a <b>demo</b>.</p></div>";
+    } else
         echo "<div id='logBtn'><a href='login.php'>Login</a><a href='register.php'>Register</a></div>";
     ?>
 
