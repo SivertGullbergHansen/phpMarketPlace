1180a434382583802aeca5de05a66251720410fc
sivertgullberghansen <sivertgh@stud.ntnu.no>
2022-01-05 19:10:38 +0100

    added documentation to file

--- config/database_info-template.php
+++ config/database_info-template.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'marketplace';
+$host = 'localhost';        // Change this to your host address (the address where your database is located) Default: 'localhost' or '127.0.0.1'
+$username = 'root';         // Username of your MySQL user  Default: 'root'
+$password = '';             // Password of your MySQL user  Default: ''
+$database = 'marketplace';  // Database-name you want to use for the marketplace. This has to correspond the database-name you set when creating the database in phpmyadmin or through SQL. Default: 'marketplace'
